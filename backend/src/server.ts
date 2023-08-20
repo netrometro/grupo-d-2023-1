f5c3cfe478ecaa8e0408029fbdd42114c27c96b7
João Lyra <joaolyra24@gmail.com>
2023-08-20 18:13:14 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import Fastify from 'fastify'
 import { FastifyInstance } from 'fastify/types/instance';
 import fastifyCors from '@fastify/cors';
 import symptomController from './symptom/symptomController';
+import allergiesController from './allergies/allergiesController';
 
 export const fastify: FastifyInstance = Fastify({
   logger: true
@@ -11,7 +12,7 @@ fastify.register(fastifyCors, {
   origin: '*',
 });
 
-fastify.register(symptomController)
+fastify.register(symptomController, allergiesController)
 
 /**
  * Run the server!
