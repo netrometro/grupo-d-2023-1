29306340b2389e745d83d39dce75be0aacb115d2
Camila Claudino <camila.claudinoo123@gmail.com>
2023-08-27 16:50:40 -0300

    criação do controller de info

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify/types/instance';
 import fastifyCors from '@fastify/cors';
 import symptomController from './symptom/symptomController';
 import allergiesController from './allergies/allergiesController';
+import infoController from './info/infoController';
 
 export const fastify: FastifyInstance = Fastify({
   logger: true
@@ -13,6 +14,7 @@ fastify.register(fastifyCors, {
 });
 
 fastify.register(symptomController, allergiesController)
+fastify.register(infoController)
 
 /**
  * Run the server!
