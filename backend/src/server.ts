9968258bf5cf427236cc14b12a37f96f33fffc77
Camila Claudino <camila.claudinoo123@gmail.com>
2023-09-15 20:12:07 -0300

    correção no server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,34 +1,31 @@
-import Fastify from 'fastify'
-import { FastifyInstance } from 'fastify/types/instance';
-import fastifyCors from '@fastify/cors';
-import symptomController from './symptom/symptomController';
-import allergiesController from './allergies/allergiesController';
-import infoController from './info/infoController';
+import Fastify from "fastify";
+import { FastifyInstance } from "fastify/types/instance";
+import fastifyCors from "@fastify/cors";
+import symptomController from "./symptom/symptomController";
+import allergiesController from "./allergies/allergiesController";
+import infoController from "./info/infoController";
 
 export const fastify: FastifyInstance = Fastify({
-  logger: true
+  logger: true,
 });
 
 fastify.register(fastifyCors, {
-  origin: '*',
+  origin: "*",
 });
 
-fastify.register(symptomController, allergiesController)
-fastify.register(infoController)
+fastify.register(symptomController, allergiesController);
+fastify.register(infoController);
 
 /**
  * Run the server!
  */
-const start = async () => {
-  try {
-    await fastify.listen({
-      port: process.env.PORT ? Number(process.env.PORT) : 3333,
-    });
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3333;
+
+fastify.listen({port: port, host:"0.0.0.0",}, function(err, address){
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-
-start() 
-
+  fastify.log.info(`server listening on ${address}`);
+});
