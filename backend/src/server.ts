e1d109467cd04c6eb22d71a65e115362641148d3
Camila Claudino <camila.claudinoo123@gmail.com>
2023-09-15 20:27:08 -0300

    fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,12 +20,9 @@ fastify.register(infoController);
  * Run the server!
  */
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3333;
-
-fastify.listen({port: port, host:"0.0.0.0",}, function(err, address){
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`server listening on ${address}`);
-});
+fastify.listen({
+  host:'0.0.0.0',
+  port:process.env.PORT ? Number(process.env.PORT) : 3333,
+}).then(() =>{
+  console.log("HTTP SERVER LISTENING STARTED")
+})
